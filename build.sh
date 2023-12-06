461f665d7912fd63b269ec5cb0aa65ca676960aa
Jacob Friis Saxberg <jacob@webcom.dk>
2023-12-06 11:55:51 +0100

    fix path

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ set -e
 WORKSPACE=/spaceinvader
 cd $WORKSPACE
 
+# make a directory for hexadecimal files
+mkdir $WORKSPACE/hex
+
 # Initialize and update git submodules
 git submodule init
 git submodule update
@@ -21,19 +24,22 @@ sed -i "s/\/\* PUBLIC_KEY_PLACEHOLDER \*\//$PUBLIC_KEY_HEX/" $WORKSPACE/apps/fir
 # Compile secure bootloader
 cd $WORKSPACE/apps/secure_bootloader/build
 make
-cp $WORKSPACE/apps/firmware/build/_build/secure_bootloader_moko.hex $WORKSPACE/apps/firmware/dfu_images
+cp $WORKSPACE/apps/secure_bootloader/build/_build/secure_bootloader_moko.hex $WORKSPACE/hex
 
 # Compile firmware
 cd $WORKSPACE/apps/firmware/build
 make
+cp $WORKSPACE/apps/firmware/build/_build/nrf52810_xxaa.hex $WORKSPACE/hex
+
+# Make hexadecimal files
+cd $WORKSPACE/hex
 
 # Generate DFU settings
-cd $WORKSPACE/apps/firmware/dfu_images
-cp $WORKSPACE/apps/firmware/build/_build/nrf52810_xxaa.hex .
 nrfutil settings generate --family NRF52810 --application nrf52810_xxaa.hex --application-version 1 --bootloader-version 1 --bl-settings-version 2 bl_settings.hex
 
 # Merge hex files
-mergehex -m bl_settings.hex secure_bootloader_moko.hex nrf52810_xxaa.hex ../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s112/hex/s112_nrf52_7.2.0_softdevice.hex --output full_dfu.hex
+cp $WORKSPACE/nRF5_SDK_17.0.2_d674dde/components/softdevice/s112/hex/s112_nrf52_7.2.0_softdevice.hex $WORKSPACE/hex
+mergehex -m bl_settings.hex secure_bootloader_moko.hex nrf52810_xxaa.hex s112_nrf52_7.2.0_softdevice.hex --output full_dfu.hex
 
 # Convert the HEX file to a BIN file
 arm-none-eabi-objcopy -I ihex -O binary full_dfu.hex $WORKSPACE/firmware.bin
